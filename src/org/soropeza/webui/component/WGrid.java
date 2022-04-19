97741174b20369f8f27045df8ac10e32ce38dcb9
Jorge Pinero <jorge.pinero@ingeint.com>
2022-04-19 14:15:49 -0400

    Fix error

--- src/org/soropeza/webui/component/WGrid.java
+++ src/org/soropeza/webui/component/WGrid.java
@@ -84,7 +84,7 @@ public class WGrid extends Grid implements ValueChangeListener, EventListener<Ev
 		if (style != null)
 			row.setStyle(style.getStyle());
 		row.setAttribute("Record_ID", Record_ID);
-		
+
 		for (int i = 0; i < columns.length; i++) {
 			if (columns[i] == null) {
 				row.appendCellChild(new Label());
@@ -97,7 +97,7 @@ public class WGrid extends Grid implements ValueChangeListener, EventListener<Ev
 				AttributeColumn attributeColumn = (AttributeColumn) columns[i];
 				attributeColumn.addValueChangeListener(this);
 				ZKUpdateUtil.setWidth(attributeColumn.getComponent(), "100%");
-				row.appendCellChild(attributeColumn.getComponent(), lstColSpan.get(i));	
+				row.appendCellChild(attributeColumn.getComponent(), lstColSpan.get(i));
 
 			} else if (columns[i] instanceof TableDirEditorColumn) {
 				TableDirEditorColumn editorColumn = (TableDirEditorColumn) columns[i];
@@ -124,17 +124,17 @@ public class WGrid extends Grid implements ValueChangeListener, EventListener<Ev
 				KeyNamePairColumn editorColumn = (KeyNamePairColumn) columns[i];
 				row.appendCellChild(editorColumn, lstColSpan.get(i));
 
-			}else if (columns[i] instanceof ToolBarButton) {
+			} else if (columns[i] instanceof ToolBarButton) {
 				ToolBarButton btn = (ToolBarButton) columns[i];
 				btn.setAttribute("Record_ID", Record_ID);
 				row.appendCellChild(btn, lstColSpan.get(i));
 
 			} else if (columns[i] instanceof Layout) {
-				Layout layout = (Layout)columns[i];
+				Layout layout = (Layout) columns[i];
 				List<Component> childrens = layout.getChildren();
 				for (Component component : childrens) {
 					if (component instanceof Button) {
-						Button btn = (Button)component;
+						Button btn = (Button) component;
 						btn.setAttribute("Record_ID", Record_ID);
 						btn.addEventListener(Events.ON_CLICK, this);
 					}
@@ -151,7 +151,7 @@ public class WGrid extends Grid implements ValueChangeListener, EventListener<Ev
 		Object source = evt.getSource();
 		WEditor editor = (WEditor) source;
 		Cell cell = (Cell) editor.getComponent().getParent();
-		
+
 		Row row = (Row) cell.getParent();
 		int iRow = row.getIndex();
 		int iColumn = getColumnIndex(cell, row);
@@ -176,6 +176,7 @@ public class WGrid extends Grid implements ValueChangeListener, EventListener<Ev
 		int Record_ID = (int) row.getAttribute("Record_ID");
 		return Record_ID;
 	}
+
 	public void setValue(Object Value, int row, int column) {
 		setValue(Value, row, column, true);
 	}
@@ -199,11 +200,11 @@ public class WGrid extends Grid implements ValueChangeListener, EventListener<Ev
 			String value = Value == null ? "" : Value.toString();
 			editor.setValue(value);
 
-		}else if (component instanceof ComboEditorBox) {
+		} else if (component instanceof ComboEditorBox) {
 			ComboEditorBox editor = (ComboEditorBox) component;
 			WSearchEditor searchEditor = (WSearchEditor) editor.getAttribute("searchEditor");
 			searchEditor.setValue(Value);
-			
+
 		}
 
 	}
@@ -230,16 +231,16 @@ public class WGrid extends Grid implements ValueChangeListener, EventListener<Ev
 		} else if (component instanceof ComboEditorBox) {
 			ComboEditorBox editor = (ComboEditorBox) component;
 			WSearchEditor searchEditor = (WSearchEditor) editor.getAttribute("searchEditor");
-			value= searchEditor.getValue();	
+			value = searchEditor.getValue();
 		} else if (component instanceof BooleanColumn) {
 			BooleanColumn editor = (BooleanColumn) component;
-			value= editor.getValue();
+			value = editor.getValue();
 		} else if (component instanceof Textbox) {
 			Textbox editor = (Textbox) component;
-			value= editor.getValue();
+			value = editor.getValue();
 		} else if (component instanceof PAttributebox) {
 			PAttributebox attributeColumn = (PAttributebox) component;
-			value = Integer.parseInt(attributeColumn.getContext());
+			value = attributeColumn.getContext() != null ? Integer.parseInt(attributeColumn.getContext()) : 0;
 		}
 		return value;
 	}
@@ -255,19 +256,20 @@ public class WGrid extends Grid implements ValueChangeListener, EventListener<Ev
 
 	@Override
 	public void onEvent(Event evt) throws Exception {
-		
+
 		if (evt.getTarget() instanceof Decimalbox) {
 			Decimalbox source = (Decimalbox) evt.getTarget();
-			NumberColumn numberColumn =(NumberColumn) source.getParent();
+			NumberColumn numberColumn = (NumberColumn) source.getParent();
 			Cell cell = (Cell) numberColumn.getParent();
 			Row row = (Row) cell.getParent();
 			int iRow = row.getIndex();
 			int iColumn = getColumnIndex(cell, row);
 			if (listener != null) {
-				GridValueChangeEvent event = new GridValueChangeEvent(source, iRow, iColumn, numberColumn.getOldValue(), source.getValue());
+				GridValueChangeEvent event = new GridValueChangeEvent(source, iRow, iColumn, numberColumn.getOldValue(),
+						source.getValue());
 				listener.gridValueChange(event);
 			}
-			
+
 		}
 
 	}
