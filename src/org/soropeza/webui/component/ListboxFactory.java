17ae06ec4d3b1e97819eb4c2e318528dcd51486c
argenis <argenismrvel@gmail.com>
2025-04-23 12:03:41 -0400

    Add support for List Box Factory

---
+++ src/org/soropeza/webui/component/ListboxFactory.java
@@ -0,0 +1,48 @@
+/******************************************************************************
+ * Copyright (C) 2008 Low Heng Sin                                            *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ *****************************************************************************/
+package org.soropeza.webui.component;
+
+import org.adempiere.webui.util.ZKUpdateUtil;
+
+/**
+ * 
+ * @author Low Heng Sin
+ *
+ */
+public class ListboxFactory {
+	
+	/**
+	 * Create new data table instance for use in custom form.
+	 * @return WListbox
+	 */
+	public static WListbox newDataTable() {
+		WListbox dataTable = new WListbox();
+		ZKUpdateUtil.setWidth(dataTable, "100%");
+		ZKUpdateUtil.setHeight(dataTable, "100%");
+		dataTable.setSizedByContent(false);
+		ZKUpdateUtil.setVflex(dataTable, true);
+		
+		return dataTable;
+	}
+	
+	/**
+	 * Create new data table instance with sized by content on.
+	 * @return WListbox
+	 */
+	public static WListbox newDataTableAutoSize() {
+		WListbox listBox = newDataTable();
+		listBox.setSizedByContent(true);
+		listBox.setSpan(true);
+		return listBox;
+	}
+}
